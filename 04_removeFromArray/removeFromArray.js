3b0b308373850db8c876a9854c6def574a68604b
kevjmay <kevinjohnmyburgh@icloud.com>
2023-12-07 21:24:51 +0000

    excercise 04 done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
 const removeFromArray = function() {
 
+    let argsArr = Array.from(arguments)
+    let first = argsArr.shift()
+    let second = argsArr.slice(0,)
+    let filteredArray = first.filter(item => !second.includes(item))
+    
+    return filteredArray
+
 };
+
+removeFromArray([1, 2, 3], 3, 4)
 
 // Do not edit below this line
 module.exports = removeFromArray;
