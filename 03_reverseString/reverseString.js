22cf3b9bf76cb0c8b337406ad5f58fe3aea32833
kevjmay <kevinjohnmyburgh@icloud.com>
2023-12-07 19:33:44 +0000

    excercise 03 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    
+    let array = string.split("")
+    let reversedArray = array.reverse()
+    let reversedString = reversedArray.join("")
 
+    return reversedString
+    
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
