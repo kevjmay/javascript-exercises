a3ab0af7ab5460d67dd977a4440d55913dc033fc
kevjmay <kevinjohnmyburgh@icloud.com>
2023-12-08 15:32:10 +0000

    excercise 07 done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,25 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+
+  let temp = (num - 32) * 5/9
+  temp = temp.toFixed(1)
+  intTemp = parseFloat(temp)
+
+  return intTemp
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+
+  let temp = ((num * 9/5) + 32);
+  temp = temp.toFixed(1)
+  intTemp = parseFloat(temp)
+
+  return intTemp
+
 };
+
+console.log(convertToCelsius(52))
+console.log(convertToFahrenheit(11.1))
 
 // Do not edit below this line
 module.exports = {
